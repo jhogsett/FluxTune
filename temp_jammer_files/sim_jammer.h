a21f130291e948d0bf57fae34f821fbb90e64854
Jerry Hogsett <jhogsett@gmail.com>
2025-06-19 13:01:16 -0700

    move unneeded files

---
+++ temp_jammer_files/sim_jammer.h
@@ -0,0 +1,25 @@
+#ifndef __SIM_JAMMER_H__
+#define __SIM_JAMMER_H__
+
+#include "async_jammer.h"
+#include "sim_transmitter.h"
+
+class SimJammer : public SimTransmitter
+{
+public:
+    SimJammer(RealizerPool *realizer_pool);
+    
+    virtual bool begin(unsigned long time, float fixed_freq);
+    virtual bool update(Mode *mode);
+    virtual bool step(unsigned long time);
+    virtual void realize();
+    
+    // Debug method to display current frequency offset
+    void debug_print_frequency() const;
+
+private:
+    AsyncJammer _jammer;
+};
+};
+
+#endif
